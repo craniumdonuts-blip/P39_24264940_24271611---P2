a5dacffea39249273a37e325087cd2e63c41c349
Angelina Laonate <ddy1155@autuni.ac.nz>
2026-05-23 20:06:14 +1200

    Create GameEventListener interface

---
+++ src/main/java/project2/view/GameEventListener.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package project2.view;
+
+import project2.model.Choice;
+import project2.model.Scene;
+import java.util.List;
+
+/**
+ *
+ * @author angie
+ */
+// implemented by view panels that need to react to game state changes
+// controller calls these
+public interface GameEventListener {
+    // used after choice is processed, view updates scene display
+    void onSceneChanged(Scene scene);
+    // used when end scene is reached, view shows endingpanel
+    void onGameOver();
+}
